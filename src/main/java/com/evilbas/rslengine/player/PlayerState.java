fe57113ff15b734b8780144f77043969c217c8c6
Evilbas <dimonloboto@gmail.com>
2021-07-06 15:01:29 -0400

    Idle state

--- src/main/java/com/evilbas/rslengine/player/PlayerState.java
+++ src/main/java/com/evilbas/rslengine/player/PlayerState.java
@@ -1,5 +1,5 @@
 package com.evilbas.rslengine.player;
 
 public enum PlayerState {
-    COMBAT, EXPLORE, INVENTORY, CRAFTING, NEW_PLAYER;
+    COMBAT, EXPLORE, INVENTORY, CRAFTING, NEW_PLAYER, IDLE;
 }
